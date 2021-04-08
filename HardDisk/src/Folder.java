a25647855c7a39b7cc54810f2aab76fe1ca91a9d
sysadmin <sysadmin@bcalab1-m11>
2021-04-08 11:48:04 +0530

    Second Commit

--- HardDisk/src/Folder.java
+++ HardDisk/src/Folder.java
@@ -1,11 +1,19 @@
 
 public class Folder {
+	
 	String name;
 	int numfiles;
 	
 Folder (String name, int numfiles){
+	
 	this.name = name;
 	this.numfiles = numfiles;
+	
 	}
+
+void AddFile(){
+	
+numfiles++;
+}
 
 }
